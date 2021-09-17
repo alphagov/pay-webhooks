2d8a8cefcd6aee934d311566a16d98cc8e475092
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2021-09-17 14:35:59 +0100

    add dropwizard boilerplate

---
+++ src/main/java/uk/gov/pay/pay-webhooksConfiguration.java
@@ -0,0 +1,10 @@
+package uk.gov.pay;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class pay-webhooksConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
