ef6bde39afaa74bcc42394f2d372d72c80af1af8
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2021-09-23 10:52:12 +0100

    PP-8443: use `live` pattern established with Ledger transactions
    Favour `live` column going forward for specifying an entities
    environment. We don't project any additional states or environments that
    domain objects could be processed in. Using a boolean simplifies
    queries, serialisation and naming conventions.

--- src/main/resources/migrations/0001_create_table_webhooks.sql
+++ src/main/resources/migrations/0001_create_table_webhooks.sql
@@ -2,17 +2,15 @@
 
 --changeset uk.gov.pay:create-table-webhooks
 
-CREATE TYPE live_or_test AS ENUM('LIVE', 'TEST');
-
 CREATE table webhooks (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     external_id VARCHAR(30) NOT NULL,
     service_id VARCHAR(30) NOT NULL,
-    type live_or_test NOT NULL,
+    live BOOLEAN NOT NULL,
     callback_url VARCHAR(2048) NOT NULL,
     description TEXT,
     status VARCHAR(32) NOT NULL
 )
 
---rollback DROP table webhooks; DROP type live_or_test
+--rollback DROP table webhooks
