de01396831b57534e52a9b4914c0cef2cbe8cf18
Dominic Jones <dominic.jones@digital.cabinet-office.gov.uk>
2021-09-29 15:31:43 +0100

    PP-8443 DB + JPA things for event subscriptions
    Adds migrations and JPA things for event subscriptions.
    
    As agreed we will use a join table to map webhooks to event
    types. Ihave modelled inJPA as one to many because I don't think
    we will ever care about tracing back from one event type
    to see all the webhooks subscribed.
    The migration includes the creation of a single event type
    (card_payment_captured). This is largely to get things off
    the ground and am happy to rename etc.
    I have used an enum to model the name of the event type,
    and then used some slightly fancy @Transient logic to
    automatically go from Enum to string and back. This means
    we can use nice type safe enum through the business logic
    and have it serialised to db correctly.
    I have used a Dropwixard DAO test to test persistence layer
    logic. I will add full integration test coverage
    once resource end of things is done.

---
+++ src/test/java/uk/gov/pay/webhooks/webhook/dao/WebhookDaoTest.java
@@ -0,0 +1,47 @@
+package uk.gov.pay.webhooks.webhook.dao;
+
+import io.dropwizard.testing.junit5.DAOTestExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.gov.pay.webhooks.eventtype.EventTypeName;
+import uk.gov.pay.webhooks.eventtype.dao.EventTypeEntity;
+import uk.gov.pay.webhooks.webhook.dao.entity.WebhookEntity;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.iterableWithSize;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class WebhookDaoTest {
+
+    public DAOTestExtension database = DAOTestExtension.newBuilder()
+            .addEntityClass(WebhookEntity.class)
+            .addEntityClass(EventTypeEntity.class)
+            .build();
+
+    private WebhookDao webhookDao;
+
+    @BeforeEach
+    public void setUp() {
+        webhookDao = new WebhookDao(database.getSessionFactory());
+    }
+
+    @Test
+    public void persistsSubscriptions() {
+        
+        WebhookEntity persisted = database.inTransaction(() -> {
+            WebhookEntity webhookEntity = new WebhookEntity();
+            EventTypeEntity eventTypeEntity = new EventTypeEntity(EventTypeName.CARD_PAYMENT_CAPTURED);
+            webhookEntity.addSubscription(eventTypeEntity);
+            return webhookDao.create(webhookEntity);
+        });
+
+        assertThat(persisted.getSubscriptions(), iterableWithSize(1));
+        assertThat(persisted.getSubscriptions(), containsInAnyOrder(any(EventTypeEntity.class)));
+        assertThat(persisted.getSubscriptions().iterator().next().getName(), is(EventTypeName.CARD_PAYMENT_CAPTURED));
+    }
+}
