710d096fe0e8c2bb59658b14a6d6ab4bb1be9291
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2021-10-05 12:39:05 +0100

    set service_id length to 32
    This is the length of service IDs created by connector. You will need to nuke your local database if you have one as this changes an existing migration.

--- src/main/resources/migrations/0001_create_table_webhooks.sql
+++ src/main/resources/migrations/0001_create_table_webhooks.sql
@@ -6,7 +6,7 @@ CREATE table webhooks (
     id SERIAL PRIMARY KEY,
     created_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     external_id VARCHAR(30) NOT NULL,
-    service_id VARCHAR(30) NOT NULL,
+    service_id VARCHAR(32) NOT NULL,
     live BOOLEAN NOT NULL,
     callback_url VARCHAR(2048) NOT NULL,
     description TEXT,
