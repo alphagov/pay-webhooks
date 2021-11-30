a45af0efa1a390e1bf0a466f42b5b98dee8ca8cc
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2021-11-30 15:44:52 +0000

    fix query and add test

--- src/test/java/uk/gov/pay/webhooks/webhook/dao/WebhookMessageDaoTest.java
+++ src/test/java/uk/gov/pay/webhooks/webhook/dao/WebhookMessageDaoTest.java
@@ -1,6 +1,5 @@
 package uk.gov.pay.webhooks.webhook.dao;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.testing.junit5.DAOTestExtension;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +13,8 @@ import uk.gov.pay.webhooks.webhook.dao.entity.WebhookEntity;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.InstantSource;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -28,7 +29,6 @@ public class WebhookMessageDaoTest {
             .build();
 
     private WebhookMessageDao webhookMessageDao;
-    private ObjectMapper objectMapper;
     private InstantSource instantSource;
 
     @BeforeEach
@@ -47,5 +47,19 @@ public class WebhookMessageDaoTest {
             return webhookMessageDao.create(webhookMessageEntity);
         });
         assertThat(persisted.getSendAt(), equalTo(Date.from(instantSource.instant())));
+    }
+
+    @Test
+    public void getsEntitySendNext() {
+        WebhookMessageEntity persisted = database.inTransaction(() -> {
+            WebhookMessageEntity webhookMessageEntity = new WebhookMessageEntity();
+            webhookMessageEntity.setSendAt(Date.from(instantSource.instant().minusMillis(1)));
+            return webhookMessageDao.create(webhookMessageEntity);
+        });
+
+        Optional<WebhookMessageEntity> next = database.inTransaction(() -> 
+                webhookMessageDao.nextToSend(Date.from(instantSource.instant())));
+        
+        assertThat(next, equalTo(Optional.of(persisted)));
     }
 }
