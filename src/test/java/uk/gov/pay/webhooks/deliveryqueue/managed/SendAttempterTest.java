c95254fbb9d2503d1c209de3918991736eb3a38a
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-01-06 12:28:13 +0000

    add tests for catching exceptions

--- src/test/java/uk/gov/pay/webhooks/deliveryqueue/managed/SendAttempterTest.java
+++ src/test/java/uk/gov/pay/webhooks/deliveryqueue/managed/SendAttempterTest.java
@@ -18,12 +18,14 @@ import uk.gov.pay.webhooks.webhook.dao.entity.WebhookEntity;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
+import java.net.http.HttpTimeoutException;
 import java.security.InvalidKeyException;
 import java.time.Instant;
 import java.time.InstantSource;
 import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.hamcrest.Matchers.is;
@@ -43,6 +45,7 @@ class SendAttempterTest {
     private InstantSource instantSource;
     private WebhookMessageDao webhookMessageDao;
     private WebhookDao webhookDao;
+    private WebhookMessageEntity webhookMessageEntity;
     
     @Mock
     private WebhookMessageSender mockWebhookMessageSender;
@@ -56,24 +59,23 @@ class SendAttempterTest {
         webhookMessageDao = new WebhookMessageDao(database.getSessionFactory());
         webhookDao = new WebhookDao(database.getSessionFactory());
         webhookDeliveryQueueDao = new WebhookDeliveryQueueDao(database.getSessionFactory(), instantSource);
+        webhookMessageEntity = new WebhookMessageEntity();
+        WebhookEntity webhookEntity = new WebhookEntity();
+        webhookEntity.setLive(true);
+        webhookEntity.setServiceId("service-id-1");
+        webhookEntity.setCreatedDate(Date.from(Instant.parse("2007-12-03T10:15:30.00Z")));
+        webhookEntity.setCallbackUrl("http://example.com");
+        webhookEntity.setSigningKey("some-signing-key");
+        webhookDao.create(webhookEntity);
+        webhookMessageEntity.setWebhookEntity(webhookEntity);
+        webhookMessageEntity.setCreatedDate(Date.from(instantSource.instant()));
     }
     
     @Test
     void sendAttempterSetsDeliveryStatusBasedOnStatusCode() throws IOException, InterruptedException, InvalidKeyException {
         given(mockWebhookMessageSender.sendWebhookMessage(any(WebhookMessageEntity.class))).willReturn(mockHttpResponse);
         
-            WebhookEntity webhookEntity = new WebhookEntity();
-            webhookEntity.setLive(true);
-            webhookEntity.setServiceId("service-id-1");
-            webhookEntity.setCreatedDate(Date.from(Instant.parse("2007-12-03T10:15:30.00Z")));
-            webhookEntity.setCallbackUrl("http://example.com");
-            webhookEntity.setSigningKey("some-signing-key");
-            webhookDao.create(webhookEntity);
-            
-            WebhookMessageEntity webhookMessageEntity = new WebhookMessageEntity();
-            webhookMessageEntity.setWebhookEntity(webhookEntity);
-            webhookMessageEntity.setCreatedDate(Date.from(instantSource.instant()));
-            var webhookMessage = webhookMessageDao.create(webhookMessageEntity);
+        var webhookMessage = webhookMessageDao.create(webhookMessageEntity);
         var sendAttempter = new SendAttempter(webhookDeliveryQueueDao, instantSource, mockWebhookMessageSender);
         var enqueuedItem = webhookDeliveryQueueDao.enqueueFrom(webhookMessage, WebhookDeliveryQueueEntity.DeliveryStatus.PENDING, Date.from(instantSource.instant()));
         given(mockHttpResponse.statusCode()).willReturn(404, 200);
@@ -83,4 +85,21 @@ class SendAttempterTest {
         sendAttempter.attemptSend(enqueuedItem);
         assertThat(enqueuedItem.getDeliveryStatus(), is(WebhookDeliveryQueueEntity.DeliveryStatus.SUCCESSFUL));
     }
+
+    @Test
+    void sendAttempterCatchesExceptions() throws IOException, InterruptedException, InvalidKeyException {
+
+        var webhookMessage = webhookMessageDao.create(webhookMessageEntity);
+        var sendAttempter = new SendAttempter(webhookDeliveryQueueDao, instantSource, mockWebhookMessageSender);
+        var enqueuedItem = webhookDeliveryQueueDao.enqueueFrom(webhookMessage, WebhookDeliveryQueueEntity.DeliveryStatus.PENDING, Date.from(instantSource.instant()));
+        given(mockWebhookMessageSender.sendWebhookMessage(any(WebhookMessageEntity.class))).willThrow(IOException.class, HttpTimeoutException.class);
+
+        assertDoesNotThrow(() -> sendAttempter.attemptSend(enqueuedItem));
+        assertThat(enqueuedItem.getDeliveryStatus(), is(WebhookDeliveryQueueEntity.DeliveryStatus.FAILED));
+
+        assertDoesNotThrow(() -> sendAttempter.attemptSend(enqueuedItem));
+        assertThat(enqueuedItem.getDeliveryStatus(), is(WebhookDeliveryQueueEntity.DeliveryStatus.FAILED));
+    }
+    
+    
 }
