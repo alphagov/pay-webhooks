738b3a06e4612e04d8ad85d3591becafe46e0895
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-01-11 11:13:00 +0000

    add singleton for WebhookMessageSender, inject SendAttempter

--- src/main/java/uk/gov/pay/webhooks/deliveryqueue/managed/WebhookMessageSendingQueueProcessor.java
+++ src/main/java/uk/gov/pay/webhooks/deliveryqueue/managed/WebhookMessageSendingQueueProcessor.java
@@ -1,6 +1,5 @@
 package uk.gov.pay.webhooks.deliveryqueue.managed;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.lifecycle.Managed;
 import io.dropwizard.setup.Environment;
 import org.hibernate.Session;
@@ -12,16 +11,9 @@ import org.slf4j.LoggerFactory;
 import uk.gov.pay.webhooks.deliveryqueue.dao.WebhookDeliveryQueueDao;
 import uk.gov.pay.webhooks.deliveryqueue.dao.WebhookDeliveryQueueEntity;
 import uk.gov.pay.webhooks.message.WebhookMessageSender;
-import uk.gov.pay.webhooks.message.WebhookMessageSignatureGenerator;
 
 import javax.inject.Inject;
-import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpTimeoutException;
-import java.security.InvalidKeyException;
-import java.time.Duration;
 import java.time.InstantSource;
-import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Optional;
 import java.util.concurrent.ScheduledExecutorService;
@@ -34,24 +26,21 @@ public class WebhookMessageSendingQueueProcessor implements Managed {
     private final InstantSource instantSource;
     private final SessionFactory sessionFactory;
     private final ScheduledExecutorService scheduledExecutorService;
-    private final ObjectMapper objectMapper;
-    private final WebhookMessageSignatureGenerator webhookMessageSignatureGenerator;
-    private final HttpClient httpClient;
+    private final SendAttempter sendAttempter;
     
     @Inject
-    public WebhookMessageSendingQueueProcessor(Environment environment, WebhookDeliveryQueueDao webhookDeliveryQueueDao, ObjectMapper objectMapper, InstantSource instantSource, SessionFactory sessionFactory, WebhookMessageSignatureGenerator webhookMessageSignatureGenerator, HttpClient httpClient) {
+    public WebhookMessageSendingQueueProcessor(Environment environment, WebhookDeliveryQueueDao webhookDeliveryQueueDao, InstantSource instantSource, SessionFactory sessionFactory, WebhookMessageSender webhookMessageSender) {
         this.webhookDeliveryQueueDao = webhookDeliveryQueueDao;
         this.instantSource = instantSource;
         this.sessionFactory = sessionFactory;
-        this.objectMapper = objectMapper;
-        this.webhookMessageSignatureGenerator = webhookMessageSignatureGenerator;
 
         scheduledExecutorService = environment
                 .lifecycle()
                 .scheduledExecutorService("retries")
                 .threads(1)
                 .build();
-        this.httpClient = httpClient;
+
+        sendAttempter = new SendAttempter(webhookDeliveryQueueDao, instantSource, webhookMessageSender);
     }
 
     @Override
@@ -85,7 +74,7 @@ public class WebhookMessageSendingQueueProcessor implements Managed {
         Transaction transaction = session.beginTransaction();
         try {
             Optional<WebhookDeliveryQueueEntity> maybeQueueItem = webhookDeliveryQueueDao.nextToSend(Date.from(instantSource.instant()));
-            maybeQueueItem.ifPresent(item -> new SendAttempter(webhookDeliveryQueueDao, instantSource, new WebhookMessageSender(httpClient, objectMapper, webhookMessageSignatureGenerator)).attemptSend(item));
+            maybeQueueItem.ifPresent(sendAttempter::attemptSend);
             transaction.commit();
         } catch (Exception e) {
             LOGGER.warn("Unexpected exception when polling queue  %s: ".formatted(e.getMessage()));
