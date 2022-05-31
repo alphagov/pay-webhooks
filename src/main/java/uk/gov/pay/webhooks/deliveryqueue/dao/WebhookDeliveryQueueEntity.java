462e28630b3d6be1f24918194f3e4825a041300c
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2022-05-31 10:53:50 +0100

    WIP: needed to rebase without db migration

--- src/main/java/uk/gov/pay/webhooks/deliveryqueue/dao/WebhookDeliveryQueueEntity.java
+++ src/main/java/uk/gov/pay/webhooks/deliveryqueue/dao/WebhookDeliveryQueueEntity.java
@@ -14,6 +14,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -90,7 +91,7 @@ public class WebhookDeliveryQueueEntity {
     @Column(name = "delivery_status")
     private String deliveryStatus;
 
-    @Column(name = "delivery_response_time")
+   @Transient 
     private Long deliveryResponseTime;
 
     public Long getDeliveryResponseTime() {
