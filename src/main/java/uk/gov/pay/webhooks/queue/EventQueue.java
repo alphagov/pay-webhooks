6f9db0cf80aba2c1ec07d283ed428bcc2c836f01
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2022-08-31 14:53:15 +0100

    BAU: move ignoring events into message service
    Currently events from the event stream are ignored when they're missing
    proprties required to check for webhook subscriptions (live and
    service_id).
    
    This change moves the logic for not handling the event into the webhook
    message service handler in favour of the code receiving and
    deserialising the event. This will mean that when the handler doesn't
    throw any exceptions, the event will be marked as processed (and
    ignored).

--- src/main/java/uk/gov/pay/webhooks/queue/EventQueue.java
+++ src/main/java/uk/gov/pay/webhooks/queue/EventQueue.java
@@ -52,15 +52,8 @@ public class EventQueue {
 
     private EventMessage getMessage(QueueMessage queueMessage) {
         try {
-            // @TODO(sfount) additional SNS message layer added to unpack the contents from SNS
-            //               this should be sense checked with the publisher (currently Ledger)
-            //               to see if this should be unpacked here or if something can be optimised with the publish
             SNSMessageDto snsMessageDto = objectMapper.readValue(queueMessage.messageBody(), SNSMessageDto.class);
             EventMessageDto eventMessageDto = objectMapper.readValue(snsMessageDto.Message(), EventMessageDto.class);
-            if (eventMessageDto.live() == null || eventMessageDto.serviceId() == null) {
-                LOGGER.warn("Unable to process events without `service_id` or `live` properties");
-                return null;
-            }
             return EventMessage.of(eventMessageDto, queueMessage);
         } catch (IOException e) {
             LOGGER.warn(
