3670611266e9685d184bdfd29be4dfa02e748a30
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2023-05-03 12:14:23 +0100

    [to be squashed]

--- src/main/resources/migrations/0013_alter_table_webhook_delivery_queue_add_on_delete_cascade.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE webhook_delivery_queue
-DROP CONSTRAINT fk_webhook_message_id,
-ADD CONSTRAINT fk_webhook_message_id FOREIGN KEY (webhook_message_id) REFERENCES webhook_messages (id) ON DELETE CASCADE;
