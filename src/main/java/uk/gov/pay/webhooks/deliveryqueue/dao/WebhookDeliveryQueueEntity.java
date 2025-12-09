6454de48f993a9eb9d715fe134c19d2a8c2c284c
Sándor Árpa <sandor.arpa@digital.cabinet-office.gov.uk>
2025-12-09 13:25:05 +0000

    Revert "PP-14767 upgrade to Dropwizard 5"

--- src/main/java/uk/gov/pay/webhooks/deliveryqueue/dao/WebhookDeliveryQueueEntity.java
+++ src/main/java/uk/gov/pay/webhooks/deliveryqueue/dao/WebhookDeliveryQueueEntity.java
@@ -1,6 +1,9 @@
 package uk.gov.pay.webhooks.deliveryqueue.dao;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import uk.gov.pay.webhooks.deliveryqueue.DeliveryStatus;
+import uk.gov.pay.webhooks.message.dao.entity.WebhookMessageEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,13 +13,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import uk.gov.pay.webhooks.deliveryqueue.DeliveryStatus;
-import uk.gov.pay.webhooks.message.dao.entity.WebhookMessageEntity;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.time.OffsetDateTime;
@@ -136,7 +136,7 @@ public class WebhookDeliveryQueueEntity {
     }
 
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "webhook_message_id", updatable = false)
     private WebhookMessageEntity webhookMessageEntity;
 
