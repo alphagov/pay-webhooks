07d80f95afb9f5d316b27ad9ee5fbdb3a26a79de
Andrzej Prochyra <andrzej.prochyra@digital.cabinet-office.gov.uk>
2026-04-08 13:02:08 +0100

    PP-15018: add tests describing logging/retry behaviour for fault responses

--- src/test/java/uk/gov/pay/webhooks/deliveryqueue/managed/SendAttempterIT.java
+++ src/test/java/uk/gov/pay/webhooks/deliveryqueue/managed/SendAttempterIT.java
@@ -1,6 +1,7 @@
 package uk.gov.pay.webhooks.deliveryqueue.managed;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.dropwizard.core.setup.Environment;
@@ -10,7 +11,14 @@ import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.pay.webhooks.deliveryqueue.DeliveryStatus;
 import uk.gov.pay.webhooks.deliveryqueue.dao.WebhookDeliveryQueueDao;
 import uk.gov.pay.webhooks.deliveryqueue.dao.WebhookDeliveryQueueEntity;
 import uk.gov.pay.webhooks.message.HttpPostFactory;
@@ -21,6 +29,7 @@ import uk.gov.pay.webhooks.validations.CallbackUrlService;
 import java.time.Instant;
 import java.time.InstantSource;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.givenThat;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
@@ -28,7 +37,13 @@ import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.http.Fault.CONNECTION_RESET_BY_PEER;
+import static com.github.tomakehurst.wiremock.http.Fault.EMPTY_RESPONSE;
+import static com.github.tomakehurst.wiremock.http.Fault.RANDOM_DATA_THEN_CLOSE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.slf4j.Logger.ROOT_LOGGER_NAME;
 import static uk.gov.pay.webhooks.deliveryqueue.dao.WebhookDeliveryQueueEntityFixture.aWebhookDeliveryQueueEntity;
@@ -36,10 +51,14 @@ import static uk.gov.pay.webhooks.message.dao.entity.WebhookMessageEntityFixture
 import static uk.gov.pay.webhooks.webhook.dao.entity.WebhookEntityFixture.aWebhookEntity;
 
 @WireMockTest
+@ExtendWith(MockitoExtension.class)
 class SendAttempterIT {
 
     @RegisterExtension
     LogCapturer logs = LogCapturer.create().captureForLogger(ROOT_LOGGER_NAME);
+
+    @Mock
+    private WebhookDeliveryQueueDao webhookDeliveryQueueDaoSpy;
 
     private static final String CALLBACK_URL = "/callback-url";
     private static final String NOW = "2022-01-01T00:00:00.000Z";
@@ -90,6 +109,65 @@ class SendAttempterIT {
         }
     }
 
+    @Test
+    void should_log_connection_reset_by_peer_fault_with_error_message() {
+        givenThat(post(CALLBACK_URL)
+                .willReturn(aResponse()
+                        .withFault(CONNECTION_RESET_BY_PEER)));
+
+        sendAttempter.attemptSend(enqueuedItem);
+
+        var loggingEvent = logs.assertContains("Exception caught by request");
+        assertThat(loggingEvent.getMarkers())
+                .singleElement()
+                .hasToString("error_message=Connection reset");
+    }
+
+    @Test
+    void should_log_random_data_then_close_fault() {
+        givenThat(post(CALLBACK_URL)
+                .willReturn(aResponse()
+                        .withFault(RANDOM_DATA_THEN_CLOSE)));
+
+        sendAttempter.attemptSend(enqueuedItem);
+
+        var loggingEvent = logs.assertContains("Exception caught by request");
+        assertThat(loggingEvent.getMarkers())
+                .singleElement()
+                .hasToString("error_message=null");
+    }
+
+    @Test
+    void should_log_empty_response_fault() {
+        givenThat(post(CALLBACK_URL)
+                .willReturn(aResponse()
+                        .withFault(EMPTY_RESPONSE)));
+
+        sendAttempter.attemptSend(enqueuedItem);
+
+        logs.assertContains("Request timed out"); // Is this the right thing?
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "CONNECTION_RESET_BY_PEER",
+            "RANDOM_DATA_THEN_CLOSE",
+            "EMPTY_RESPONSE"
+    })
+    void should_enqueue_for_retry_on_fault(Fault fault) {
+        givenThat(post(CALLBACK_URL)
+                .willReturn(aResponse()
+                        .withFault(fault)));
+
+        sendAttempter.attemptSend(enqueuedItem);
+
+        Mockito.verify(webhookDeliveryQueueDaoSpy)
+                .enqueueFrom(
+                        any(),
+                        eq(DeliveryStatus.PENDING),
+                        argThat(instant -> instant.isAfter(Instant.parse(NOW))));
+    }
+
     private static WebhookDeliveryQueueEntity buildWebhookDeliveryQueueEntityWithCallbackURL(String callbackUrl) {
         var webhook = aWebhookEntity()
                 .withCallbackUrl(callbackUrl)
@@ -102,13 +180,12 @@ class SendAttempterIT {
                 .build();
     }
 
-    private static @NonNull SendAttempter buildSendAttempter() {
-        var instantSource = InstantSource.fixed(Instant.parse("2022-01-01T00:00:00Z"));
+    private @NonNull SendAttempter buildSendAttempter() {
+        var instantSource = InstantSource.fixed(Instant.parse(NOW));
         var webhookMessageSender = buildWebhookMessageSender();
         var environment = new Environment("fake-environment");
-        var webhookDeliveryQueueDao = mock(WebhookDeliveryQueueDao.class);
         return new SendAttempter(
-                webhookDeliveryQueueDao,
+                webhookDeliveryQueueDaoSpy,
                 instantSource,
                 webhookMessageSender,
                 environment
